f14273731ee778b1587e28eada4b2b3a3e863b29
Shakil-a <101872079+Shakil-a@users.noreply.github.com>
2022-08-05 14:05:31 +0100

    1st commit

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,31 @@ const User = require('./User');
 const Scooter = require('./Scooter');
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor (scooterSessions){
+    this.scooterSessions = scooterSessions
+    this.users = []
+  }
+register(userName, password, age){
+  for (let i = 0; i > this.users.length; i++){
+    if ( userName == this.users[i] ){
+      return "this username is already taken"
+    }
+  }
+const user = new User(userName, password, age)
+this.users.push(user)
+return user
+
 }
+
+
+  
+       
+}
+const app = new ScooterApp()
+const shakil = app.register('shakil', 'something', 15)
+console.log(app)
+console.log(shakil)
+ 
+ 
 
 module.exports = ScooterApp;
