19276bb7be2f51610e8083c817bc900330e7cf47
Shakil-a <101872079+Shakil-a@users.noreply.github.com>
2022-08-05 14:06:09 +0100

    changes

--- src/User.js
+++ src/User.js
@@ -1,5 +1,14 @@
-class User {
-    // User code here
+  class User {
+    //ive just made the requirements for the user class, this may change at one point
+    constructor(userName, password, age) {
+      this.userName = userName 
+      this.password = password
+      this.age = age
+
+    }
+
   }
+
+
   
   module.exports = User
